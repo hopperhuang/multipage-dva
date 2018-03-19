7d079fc13ec8c6ff8693095c419ca3b3bc453a6f
黄家炜 <huangjiawei@innotechx.com>
2018-03-19 17:24:37 +0800

    1.Feature: config svg loader

--- webpack.config.js
+++ webpack.config.js
@@ -206,6 +206,14 @@ module.exports = {
         },
       },
       { test: /\.ejs$/, loader: 'ejs-loader' },
+      {
+        test: /\.(svg)$/i,
+        loader: 'svg-sprite-loader',
+        include: [
+          require.resolve('antd-mobile').replace(/warn\.js$/, ''),  // 1. 属于 antd-mobile 内置 svg 文件
+          path.resolve(__dirname, 'src/components'),  // 自己私人的 svg 存放目录
+        ],
+      },
     ],
   },
   devtool: 'inline-source-map',
