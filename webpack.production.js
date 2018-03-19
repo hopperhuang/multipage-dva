20c5408078d0c35ba9301414b6230e4c00db0dea
黄家炜 <huangjiawei@innotechx.com>
2018-03-19 17:40:21 +0800

    1.Feature: config svg loader, now can read all folder in src components

--- webpack.production.js
+++ webpack.production.js
@@ -233,7 +233,7 @@ module.exports = {
         loader: 'svg-sprite-loader',
         include: [
           require.resolve('antd-mobile').replace(/warn\.js$/, ''),  // 1. 属于 antd-mobile 内置 svg 文件
-          path.resolve(__dirname, 'src/components'),  // 自己私人的 svg 存放目录
+          path.resolve(__dirname, 'src'),  // 自己私人的 svg 存放目录
         ],
       },
     ],
